889b281664b57da9110b6b7ec32f3bcac1b0692f
Shakya Rathnaweera <59193561+ShakyaPr@users.noreply.github.com>
2022-06-01 23:07:40 +0530

    final:v1.0

---
+++ app/src/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "[INFO] waiting five seconds to docker DNS to be stabilized"
+FLASK_PORT=8000
+WORKER_THREADS=4
+CERT_LOC=/service/src/appkey.crt
+KEY_LOC=/service/src/appkey.key
+
+echo "[INFO] starting Web Server"
+
+gunicorn \
+-b 0.0.0.0:$FLASK_PORT \
+-w $WORKER_THREADS \
+--certfile $CERT_LOC \
+--keyfile $KEY_LOC \
+--reload server:app
